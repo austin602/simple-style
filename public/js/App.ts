8761ee046eca138ffa84a155c985760568b379e2
austin landon <austin.m.landon@gmail.com>
2016-11-16 16:40:22 -0700

    designing

---
+++ public/js/App.ts
@@ -0,0 +1,18 @@
+namespace App {
+    let app = angular.module ('App' , ['ui.router']);
+
+    app.config ([
+        '$stateProvider' ,
+        ($stateProvider) => {
+
+            $stateProvider
+                .state  ('home', {
+                    url: '/',
+                    templateUrl: 'templates/home.html',
+                    controller: App.HomeController,
+                    controllerAs: 'HomeController'
+                })
+
+        }
+    ]);
+}
