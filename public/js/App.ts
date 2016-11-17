21d08aee1df127ee9c2c3919491dfa834867c66e
austin landon <austin.m.landon@gmail.com>
2016-11-17 13:54:06 -0700

    added seperate pages

--- public/js/App.ts
+++ public/js/App.ts
@@ -1,18 +1,30 @@
 namespace App {
-    let app = angular.module ('App' , ['ui.router']);
+    let app = angular.module ('App', ['ui.router']);
 
     app.config ([
-        '$stateProvider' ,
+        '$stateProvider',
         ($stateProvider) => {
 
             $stateProvider
-                .state  ('home', {
+                .state ('home', {
                     url: '/',
                     templateUrl: 'templates/home.html',
                     controller: App.HomeController,
-                    controllerAs: 'HomeController'
+                    controllerAs: 'homeController'
                 })
+                .state ('product', {
+                    url: '/product',
+                    templateUrl: 'templates/product.html',
+                    controller: App.ProductController,
+                    controllerAs: 'productController'
+                })
+                .state ('front-page', {
+                    url:'/front-page',
+                    templateUrl:'templates/front-page.html',
+                    controller: App.FrontpageController,
+                    controllerAs: 'frontpageController'
 
+                })
         }
     ]);
 }
